b6c0cb05e979a17ac12206395c2fd77f34af1ad0
BartoszSz80 <bartek@bartweb.eu>
2017-03-29 09:43:14 +0200

    Add files via upload

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,8 +37,9 @@ module.exports = function(grunt) {
     // Load the plugins tasks
     grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
-
+	grunt.loadNpmTasks('grunt-contrib-watch');
     // Default task(s).
 	
-    grunt.registerTask('default', ['sass', 'imagemin']);
+    grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+	
 };
